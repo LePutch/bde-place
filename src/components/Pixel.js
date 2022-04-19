396066b11efbbb7e4b851de47d76d1ca29bf7070
Jérémy Pucci <jeremy.pucci@etu.univ-grenoble-alpes.fr>
2022-04-19 14:41:15 +0200

    improve perf

--- src/components/Pixel.js
+++ src/components/Pixel.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { memo } from "react";
 import "./pixel.css"
 import { useSelectedPixelState } from "./useSelectedPixelState";
 
 export const Pixel = ({ id }) => {
 
     const [isSelected, color, dispatch] = useSelectedPixelState(id)
+    return <ThemedPixel isSelected={isSelected} color={color} dispatch={dispatch} id={id} />
+}
+
+const ThemedPixel = memo(({ id, isSelected, color, dispatch }) => {
     const baseStyle = { width: 1, height: 1, background: "grey" }
     const style = isSelected ? { ...baseStyle, background: color } : baseStyle
 
@@ -16,4 +20,4 @@ export const Pixel = ({ id }) => {
         }}
         className={isSelected ? "selected" : ""}
     />
-}
+})
