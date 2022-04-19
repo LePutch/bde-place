29bd3791eecaab0b57d02fca64ca38aba027720f
Jérémy Pucci <jeremy.pucci@etu.univ-grenoble-alpes.fr>
2022-04-19 15:23:40 +0200

    add pan and pitch

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import { ColorPicker } from './components/ColorPicker';
 import { Pixel } from './components/Pixel'
 import { RANGE } from './config/grid';
 import { PixelProvider } from './features/selectPixel';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import "./grid.css";
 
 const App = () => (
   <PixelProvider>
     <div className='layout'>
-      <div className='grid'>
-        <div className='scale'>
-          {RANGE.map((i) => <div key={`line-${i}`} style={{ display: "flex" }}>
-            {RANGE.map((j) => <Pixel key={i + ':' + j} id={i + ':' + j} color="red" />)}
-          </div>)}
-        </div>
-      </div>
+      <TransformWrapper minScale={2} maxScale={50}>
+        <TransformComponent wrapperClass='grid-wrapper'>
+          <div className='grid'>
+            {RANGE.map((i) => <div key={`line-${i}`} style={{ display: "flex" }}>
+              {RANGE.map((j) => <Pixel key={i + ':' + j} id={i + ':' + j} color="red" />)}
+            </div>)}
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
       <ColorPicker />
     </div>
   </PixelProvider>
